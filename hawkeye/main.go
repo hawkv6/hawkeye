62eca806a1841e84d84a29a0f52991797c42f8ef
severindellsperger <severin.dellsperger@gmail.com>
2024-07-22 14:12:55 +0200

    fix lint errors

--- hawkeye/main.go
+++ hawkeye/main.go
@@ -2,13 +2,6 @@ package main
 
 import "github.com/hawkv6/hawkeye/hawkeye/cmd"
 
-var (
-	jagwServiceAddress   string
-	jagwRequestPort      string
-	jagwSubscriptionPort string
-	grpcPort             string
-)
-
 func main() {
 	cmd.Execute()
 }
