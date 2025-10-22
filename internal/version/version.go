869e68891eb4cc96fd8063e80297b39dfdfae5ce
INS <ins@dels-ibn-jagw.network.garden>
2025-10-22 14:57:28 +0000

    adjust version to v1.0.1

--- internal/version/version.go
+++ internal/version/version.go
@@ -4,7 +4,7 @@ import "fmt"
 
 const MAJOR uint = 1
 const MINOR uint = 0
-const PATCH uint = 0
+const PATCH uint = 1
 
 func GetVersion() string {
 	return fmt.Sprintf("%d.%d.%d", MAJOR, MINOR, PATCH)
